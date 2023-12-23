3788ea8218ce1085282b6a7b4bf4e5468f29a00a
Avuthar <154669767+Avuthar@users.noreply.github.com>
2023-12-23 22:16:53 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const colorWave = document.getElementById('color-wave');
+const saveButton = document.getElementById('save-button');
+
+let currentColor1 = '#ff9a9e';
+let currentColor2 = '#fad0c4';
+
+function changeColor() {
+    currentColor1 = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    currentColor2 = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    colorWave.style.background = `linear-gradient(45deg, ${currentColor1} 0%, ${currentColor2} 100%)`;
+}
+
+setInterval(changeColor, 1000); 
+
+saveButton.addEventListener('click', function() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 1920;
+    canvas.height = 1080;
+    const ctx = canvas.getContext('2d');
+
+    // Create a gradient similar to the current background
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+    gradient.addColorStop(0, currentColor1);
+    gradient.addColorStop(1, currentColor2);
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Create a link and set it to the image URL, then click it to download
+    const link = document.createElement('a');
+    link.download = 'color-wave-background.jpg';
+    link.href = canvas.toDataURL('image/jpeg', 0.8); // Quality from 0 to 1
+    link.click();
+});
+
